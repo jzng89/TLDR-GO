918c83e02206a87b62a400f5397159e2604d39c5
Ng Jian Zhen <31285049+jzng89@users.noreply.github.com>
2018-12-15 19:30:55 +0800

    Add files via upload

--- mainserver.go
+++ mainserver.go
@@ -10,30 +10,41 @@ import (
 	"strings"
 )
 
+//Page title
 type Page struct {
 	Title string
 }
 
-//-------------------------------------------------------------------------------------//
+//------------------------------Global Variables-------------------------------------//
 //Compile templates on start
-var templ = func() *template.Template {
-	t := template.New("")
+var templ = ParseTemplates()
+
+//ParseTemplates use for multi directory html parsing
+func ParseTemplates() *template.Template {
+	t := template.New("Base")
 	err := filepath.Walk("./", func(path string, info os.FileInfo, err error) error {
 		if strings.Contains(path, ".html") {
-			fmt.Println(path)
 			_, err = t.ParseFiles(path)
+			fmt.Println(path)
 			if err != nil {
+				logger.Println(err)
 				fmt.Println(err)
 			}
 		}
+
 		return err
 	})
 
 	if err != nil {
 		panic(err)
 	}
+
 	return t
-}()
+}
+
+//logging
+var errorlog *os.File
+var logger *log.Logger
 
 //---------------------------------------Page Handlers----------------------------------//
 //Handler for homepage
@@ -41,6 +52,8 @@ func homepageHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Home")
 	err := templ.ExecuteTemplate(w, "index", &Page{Title: "Welcome to TL;DR"})
 	if err != nil {
+		fmt.Println(err)
+		logger.Println(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
@@ -50,38 +63,33 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("About")
 	err := templ.ExecuteTemplate(w, "about", &Page{Title: "About TL;DR"})
 	if err != nil {
+		fmt.Println(err)
+		logger.Println(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-//Handler for test Page
-func testHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		fmt.Println("Test")
-		testT, _ := template.ParseFiles("static/test.html")
-		testT.Execute(w, nil)
+//Server log to file
+func init() {
+	errorlog, err := os.OpenFile("serverlog.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Printf("Error opening file: %v", err)
+		os.Exit(1)
 	}
+	log.SetOutput(errorlog)
+	logger = log.New(errorlog, "\r\nTDLR : ", log.Lshortfile|log.LstdFlags)
 }
 
 func main() {
 	//--------------------------------------Routers-------------------------------------//
 	http.HandleFunc("/", homepageHandler)
+	http.HandleFunc("/index", homepageHandler)
 	http.HandleFunc("/about", aboutHandler)
-	http.HandleFunc("/test", testHandler)
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	//---------------------------------------------------------------------------------//
-
-	//log to file
-	f, err := os.OpenFile("serverlog.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatalf("Error opening file: %v", err)
-	}
-	defer f.Close()
-	logger := log.New(f, "Logged : ", log.LstdFlags)
-	log.SetOutput(f)
-
 	//start server
+	fmt.Println("Starting server on port 9090")
 	logger.Println("Starting server on port 9090")
 	logger.Fatal(http.ListenAndServe(":9090", nil))
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,16 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-var SideNavTog = true;
+var SideNavTog = false;
 
 function toggleSideNav(){
   if (SideNavTog) {
     document.getElementById("leftsidenav").style.width = "0";
     document.getElementById("mainContent").style.marginLeft = "0";
+    document.getElementById("mainfooter").style.marginLeft = "0";
     SideNavTog = false;
   }else{
     document.getElementById("leftsidenav").style.width = "250px";
     document.getElementById("mainContent").style.marginLeft = "280px";
+    document.getElementById("mainfooter").style.marginLeft = "280px";
     SideNavTog = true;
   }
 }
@@ -28,24 +30,22 @@ function scrollFunction() {
   // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
   if (window.pageYOffset >= sticky) {
     topnav.classList.add("stickyTopNav");
-    topnav.classList.add("stickyTopNav");
     navdrop.classList.add("stickydropdown");
     navdrop.style.position = "fixed";
-    sidenav.classList.add("notransition");
-    sidenav.style.position = "fixed";
-    sidenav.style.top = "-230px";
-    sidenav.offsetHeight; // Trigger a reflow, flushing the CSS changes
-    sidenav.classList.remove("notransition");
+    //sidenav.classList.add("notransition");
+    //sidenav.style.position = "fixed";
+    //sidenav.style.top = "-230px";
+    //sidenav.offsetHeight; // Trigger a reflow, flushing the CSS changes
+    //sidenav.classList.remove("notransition");
   } else {
     topnav.classList.remove("stickyTopNav");
     navdrop.classList.remove("stickydropdown");
     navdrop.style.position = "absolute";
-    sidenav.classList.add("notransition");
-    sidenav.style.transition = "none !important";
-    sidenav.style.position = "absolute";
-    sidenav.style.top = "0";
-    sidenav.offsetHeight; // Trigger a reflow, flushing the CSS changes
-    sidenav.classList.remove("notransition");
+    //sidenav.classList.add("notransition");
+    //sidenav.style.position = "absolute";
+    //sidenav.style.top = "0";
+    //sidenav.offsetHeight; // Trigger a reflow, flushing the CSS changes
+    //sidenav.classList.remove("notransition");
   }
 
   /* this code is under consideration for minimized header
